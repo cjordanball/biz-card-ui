56a4d30afac392c0174164b21c8f1a79aa4d89f3
Jordan Ball <cjordanball@ballwebdev.com>
2021-07-18 18:55:26 -0400

    initial commit

---
+++ biz-card-ui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  biz-card-ui
+//
+//  Created by C. Jordan Ball III on 7/18/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ biz-card-ui/biz_card_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  biz_card_uiApp.swift
+//  biz-card-ui
+//
+//  Created by C. Jordan Ball III on 7/18/21.
+//
+
+import SwiftUI
+
+@main
+struct biz_card_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
